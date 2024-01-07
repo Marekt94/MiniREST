187c0ba196c47e0b07a70c8cc7b0942aa9104260
Marekt94 <marekt94@gmail.com>
2024-01-07 16:13:16 +0100

    Interface to SSL, refactor server with SSL object

--- MiniREST.Server.Intf.pas
+++ MiniREST.Server.Intf.pas
@@ -6,6 +6,17 @@ uses SysUtils, MiniREST.Controller.Intf, {MiniREST.Controller.Base,}
   MiniREST.Controller.Security.Intf, MiniREST.Intf;
 
 type
+  ISSL = interface
+    ['{B707B0D3-D358-4CFA-B4B2-DEB66D136E4E}']
+    procedure SetCertPath(const ACertPath : string);
+    procedure SetKeyPath(const AKeyPAth : string);
+    function GetSecured : boolean;
+    procedure SetSecured(const ASecured : boolean);
+    property CertPath : string write SetCertPath;
+    property KeyPath  : string write SetKeyPath;
+    property Secured  : boolean read GetSecured write SetSecured;
+  end;
+
   IMiniRESTServer = interface
   ['{91170691-E5BF-4C74-9B7F-052908DDA8E7}']
     procedure AddController(AController : TClass); overload;
